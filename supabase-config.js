2df9eeccaecba159a0eece5ba23e3613c9146599
kerols16 <kerolshany16@gmail.com>
2026-08-25 22:13:37 +0300

    fix experience title

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://wvbcqgnhdltodoupblbg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2YmNxZ25oZGx0b2RvdXBibGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMTU1OTEsImV4cCI6MjEwMDY5MTU5MX0.kK0HfbwkgP2LxNh-EfBBcWw-2R4IUKoO80RXb5-3Lig";
 
 // دالة عامة لجلب أي جدول من Supabase
-async function fetchFromSupabase(table, query = "") {
+export async function fetchFromSupabase(table, query = "") {
     const url = `${SUPABASE_URL}/rest/v1/${table}?${query}`;
     const res = await fetch(url, {
         headers: {
